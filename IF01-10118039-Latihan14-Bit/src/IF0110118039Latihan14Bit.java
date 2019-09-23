22a3e39070b3a4e631da64eba995da0ca42089b2
fathsbs <55565303+fathsbs@users.noreply.github.com>
2019-09-23 20:11:54 +0700

    Add files via upload

---
+++ IF01-10118039-Latihan14-Bit/src/IF0110118039Latihan14Bit.java
@@ -0,0 +1,36 @@
+/*
+ * NAMA              : FATAHILLAH SATRIA BIMA SENO
+ * KELAS             : IF-01
+ * NIM               : 10118039
+ * DESKRIPSI PROGRAM :   
+ */
+
+public class IF0110118039Latihan14Bit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 60;
+        int b = 13;
+        int c = 0;
+        c = a & b;
+        // 12 = 0000 1100
+        System.out.println("a & b = " + c);
+        c = a | b;
+        // 61 = 0011 1101
+        System.out.println("a | b = " + c);
+        c = a ^ b;
+        // 49 = 0011 0001
+        System.out.println("a ^ b = " + c);
+        c = ~a;
+        // -61 = 1100 0011
+        System.out.println(" ~a = " + c);
+        c = a << 2;
+        // 240 = 1111 0000
+        System.out.println("a << 2= " + c);
+        c = a >> 2;
+        // 15 = 0000 1111
+        System.out.println("a >> 2 = " + c);
+    }
+}
